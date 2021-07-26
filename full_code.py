488c29a8df55bc5de20430c5208d3eedbe538088
JustiBonhof <justinbonhof@gmail.com>
2021-07-26 20:04:08 +0200

    Changed files!

--- full_code.py
+++ full_code.py
@@ -8,7 +8,11 @@
 import speech_recognition as sr
 import pyttsx3
 import pywhatkit
-
+from youtubesearchpython import VideosSearch
+import vlc
+import pafy
+import youtube_dl
+import os
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
@@ -31,20 +35,52 @@ def take_command():
             if "happy" in command:
                 command = command.replace("happy", "")
     except:
-       pass
+        pass
     return command
 
 
+def download_video(url):
+    video = url.strip()
+    ydl = youtube_dl.YoutubeDL()
+    try:
+        ydl_opts = {
+            'outtmpl': "test.mp3",
+            # 'outtmpl': unicode(customName),
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([video])
+    except:
+        print("Invalid URL!")
+
+
+def check_download():
+    if os.path.exists("test.mp3"):
+        os.remove("test.mp3")
+        print("removed copy")
+
+
 def run_happy():
+    check_download()
     command = take_command()
     print(command)
     if "play" in command:
         song = command.replace("play", "")
         talk("playing " + song)
-        pywhatkit.playonyt(song, use_api=True)
+        video_search = VideosSearch(song, limit=1)
+        url = "https://www.youtube.com/watch?v=" + video_search.result()['result'][0]['id']
+        download_video(url)
+        video = pafy.new(url)
+        best = video.getbest()
+        media = vlc.MediaPlayer(best.url)
+        media.play()
+        print(url)
+        # pywhatkit.playonyt(song, use_api=True)
 
 
 run_happy()
-
-
-
